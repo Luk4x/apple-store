3da60c2da05ef778eb1b0551897348c973d1b028
Luk4x <luk4xm4ci3l@gmail.com>
2023-02-08 23:18:25 -0300

    desktop socials

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,12 +9,22 @@ import {
     HiOutlineBars3,
     HiOutlineBars3BottomLeft
 } from 'react-icons/hi2';
-import { SiApple } from 'react-icons/si';
-import { Popover, Transition } from '@headlessui/react';
+import { BsGridFill, BsApple } from 'react-icons/bs';
+import {
+    FaLinkedinIn,
+    FaGithub,
+    FaWhatsapp,
+    FaTelegramPlane,
+    FaInstagram,
+    FaDiscord
+} from 'react-icons/fa';
+import { Popover, Menu, Transition } from '@headlessui/react';
+import toast from 'react-hot-toast';
 
 import { useSelector } from 'react-redux';
 import { selectCartProducts } from '../redux/cartSlice';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { IoShareSocial } from 'react-icons/io5';
 
 export default function Header() {
     const { data: session } = useSession();
@@ -83,15 +93,85 @@ export default function Header() {
                         </>
                     )}
                 </Popover>
-                <Link
-                    href="/"
-                    className={`${
-                        blur ? 'translate-x-[150px] text-2xl' : 'text-4xl'
-                    } cursor-pointer opacity-75 transition duration-[450ms] hover:opacity-100`}
-                    onClick={() => blur && handleMobileMenu()}
-                >
-                    <SiApple />
+                <Link href="/" onClick={() => blur && handleMobileMenu()}>
+                    <BsApple
+                        className={`${
+                            blur && 'translate-x-[164px]'
+                        } headerIcon cursor-pointer opacity-75 transition duration-[450ms] hover:opacity-100`}
+                    />
                 </Link>
+                <Menu as="div" className="relative">
+                    <Menu.Button>
+                        <BsGridFill className="headerIcon hidden translate-y-[2px] md:inline" />
+                    </Menu.Button>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-500"
+                        enterFrom="opacity-0 -translate-y-2"
+                        enterTo="opacity-100 -translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 -translate-y-0"
+                        leaveTo="opacity-0 -translate-y-2"
+                    >
+                        <Menu.Items className="menuList">
+                            <div className="w-2/4 space-y-1">
+                                <Menu.Item>
+                                    <a
+                                        href="https://www.linkedin.com/in/lucasmacielf/"
+                                        target="_blank"
+                                    >
+                                        <FaLinkedinIn className="text-[#0077B5]" />
+                                        <p>LinkedIn</p>
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <a href="https://github.com/Luk4x/" target="_blank">
+                                        <FaGithub className="text-[#F2F2F2]" />
+                                        <p>Github</p>
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <a
+                                        href="https://wa.me/5522996112570?text=Yo%20Lucas,%20vim%20pela%20Apple%20Store."
+                                        target="_blank"
+                                    >
+                                        <FaWhatsapp className="text-[#45c655]" />
+                                        <p>WhatsApp</p>
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <a href="https://t.me/lu_k4x" target="_blank">
+                                        <FaTelegramPlane className="text-[#25a3e1]" />
+                                        <p>Telegram</p>
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <a
+                                        href="https://www.instagram.com/lu_k4x/"
+                                        target="_blank"
+                                    >
+                                        <FaInstagram className="text-[#E4405F]" />
+                                        <p>Instagram</p>
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <button
+                                        onClick={() => {
+                                            navigator.clipboard.writeText('Luk4x#5068');
+                                            toast.success('Discord copiado!', {
+                                                position: 'bottom-center'
+                                            });
+                                        }}
+                                    >
+                                        <FaDiscord className="text-[#545fef]" />
+                                        <p>Discord</p>
+                                    </button>
+                                </Menu.Item>
+                            </div>
+                            <IoShareSocial className="h-24 w-24 text-violet-600" />
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </div>
             <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
                 <Link href="/" className="headerLink">
@@ -127,8 +207,8 @@ export default function Header() {
                         <div className="tooltip before:content-['LogOut']">
                             <Image
                                 src={session.user?.image || '/user-image.png'}
-                                width={35}
-                                height={35}
+                                width={36}
+                                height={36}
                                 alt="User Image"
                                 className=" cursor-pointer rounded-full object-contain "
                                 onClick={() => signOut()}
